5a24e71318f1da468d38e2dd3c8b394fdaa60f3b
Felipe Agustin Robalino Sosa <felipe.robalino@udla.edu.ec>
2024-10-22 19:08:55 -0500

    app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ def verify():
     token = request.args.get('hub.verify_token')
     challenge = request.args.get('hub.challenge')
 
-    if mode == 'subscribe' and token == 'mi_token_de_verificacion':  # Token de verificación
+    if mode == 'subscribe' and token == 'mi_token_de_verificacion':  # Asegúrate de que este token coincide
         return challenge, 200
     else:
         return "Error de verificación", 403
@@ -19,7 +20,7 @@ def verify():
 def webhook():
     print("Solicitud POST recibida")
     data = request.json  # Captura el JSON recibido
-    print(f"Datos recibidos: {data}")  # Imprime los datos recibidos para verlos en los logs
+    print(f"Datos recibidos: {data}")  # Imprime los datos para ver qué está llegando
 
     if data and 'entry' in data:
         for entry in data['entry']:
@@ -33,4 +34,5 @@ def webhook():
         return "Error: No se pudo procesar el webhook", 400
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Obtiene el puerto desde la variable de entorno PORT
+    app.run(host='0.0.0.0', port=port)  # Asegura que Flask escuche en el puerto especificado
