dc21a5b29239fb8cfa7d90f318094c83dbd0ace5
Felipe Agustin Robalino Sosa <felipe.robalino@udla.edu.ec>
2024-12-18 16:33:29 -0500

    API estados impresion

--- api/database.py
+++ api/database.py
@@ -6,7 +6,7 @@ def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = (
         "postgresql://ranpubackenddatabase_user:"
         "pKqQ12aSzKeFiqvsR5pUEUFipbWWcgox"
-        "@dpg-ctgraj0gph6c73ck0ppg-a:5432/ranpubackenddatabase"
+        "@dpg-ctgraj0gph6c73ck0ppg-a.oregon-postgres.render.com:5432/ranpubackenddatabase"
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
