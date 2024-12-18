b9bb6e2e337d8430f9dd3affb06980833452f22a
Felipe Agustin Robalino Sosa <felipe.robalino@udla.edu.ec>
2024-12-18 15:55:24 -0500

    Pruebas impresoras

--- fastapi_app/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://ranpubackenddatabase_user:pKqQl2aSzKeFiqvsR5pUEUFipbWWcgox@dpg-ctgraj0gph6c73ck0ppg-a:5432/ranpubackenddatabase"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
--- fastapi_app/models/postgres_models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from fastapi_app.database import Base
-
-class EstadoImpresora(Base):
-    __tablename__ = 'estado_impresora'
-
-    estado_impresora_id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String(50), nullable=False)
