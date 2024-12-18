587470c2d2c444692602fb2cfe35ad4128c8797f
Felipe Agustin Robalino Sosa <felipe.robalino@udla.edu.ec>
2024-12-18 17:21:11 -0500

    pruebas de conexion

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from .database import db
 class EstadosImpresoras(db.Model):
     __tablename__ = "estados_impresoras"
 
-    estado_impresora_id = db.Column(db.Integer, primary_key=True)
+    estado_impresora_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(50), nullable=False)
 
     def to_dict(self):
