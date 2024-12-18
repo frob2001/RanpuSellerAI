9cd1a740392d7caa1eb5a4f27261b4789190cc8e
Felipe Agustin Robalino Sosa <felipe.robalino@udla.edu.ec>
2024-12-18 15:28:56 -0500

    Pruebas impresoras

--- fastapi_app/database.py
+++ fastapi_app/database.py
@@ -1,14 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Configuración de la base de datos
-DATABASE_URL = "postgresql://ranpubackenddatabase_user:pKqQl2aSzKeFiqvsR5pUEUFipbWWcgox@dpg-ctgraj0gph6c73ck0ppg-a.oregon-postgres.render.com/ranpubackenddatabase"
+DATABASE_URL = "postgresql://ranpubackenddatabase_user:pKqQl2aSzKeFiqvsR5pUEUFipbWWcgox@dpg-ctgraj0gph6c73ck0ppg-a:5432/ranpubackenddatabase"
 
-# Crear la instancia del motor de SQLAlchemy
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Declaración de la clase base
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()
