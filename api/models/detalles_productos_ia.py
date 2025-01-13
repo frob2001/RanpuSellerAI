b2557bc76b868030f9652bf18fb056d1e4442751
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2025-01-12 21:06:18 -0500

    Add scale to detalles productos ia

--- api/models/detalles_productos_ia.py
+++ api/models/detalles_productos_ia.py
@@ -11,6 +11,7 @@ class DetallesProductosIA(db.Model):
     obj_downloadable_url = db.Column(db.String(5000), nullable=True)
     url_expiring_date = db.Column(db.DateTime, nullable=True)
     origin_task_id = db.Column(db.String(200), nullable=True)
+    scale = db.Column(db.Numeric(5, 2), nullable=False)
 
     def to_dict(self):
         return {
@@ -21,5 +22,6 @@ class DetallesProductosIA(db.Model):
             "is_in_order": self.is_in_order,
             "obj_downloadable_url": self.obj_downloadable_url,
             "url_expiring_date": self.url_expiring_date,
-            "origin_task_id": self.origin_task_id
+            "origin_task_id": self.origin_task_id,
+            "scale": float(self.scale)
         }
